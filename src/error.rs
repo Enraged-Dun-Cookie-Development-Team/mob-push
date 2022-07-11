1bb53ac53ae3fae82f91ce85872e26e09bb9bc91
Frozen String <964413011@qq.com>
2022-07-11 10:47:17 +0800

    ➕ (json) 添加json 序列化支持

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,8 @@ where
 {
     Manage(M::Err),
     Reqwest(reqwest::Error),
+    Mob { state: u16, msg: String },
+    Json(serde_json::Error),
 }
 
 impl<M> std::fmt::Debug for MobPushError<M>
@@ -14,8 +16,14 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Manage(arg0) => f.debug_tuple("Manage").field(arg0).finish(),
-            Self::Reqwest(arg0) => f.debug_tuple("Reqwest").field(arg0).finish(),
+            Self::Manage(err) => f.debug_tuple("Manage").field(err).finish(),
+            Self::Reqwest(err) => f.debug_tuple("Reqwest").field(err).finish(),
+            MobPushError::Mob { state, msg } => f
+                .debug_struct("Mob")
+                .field("state", state)
+                .field("msg", msg)
+                .finish(),
+            MobPushError::Json(err) => f.debug_tuple("Json").field(err).finish(),
         }
     }
 }
@@ -25,6 +33,8 @@ impl<M: UserSubscribeManage> std::fmt::Display for MobPushError<M> {
         match self {
             MobPushError::Manage(err) => write!(f, "Subscribe Manage Error : {err}"),
             MobPushError::Reqwest(err) => write!(f, "Reqwest Error : {err}"),
+            MobPushError::Mob { state, msg } => write!(f, "Mob Pusher Error : [{}] {}", state, msg),
+            MobPushError::Json(err) => write!(f, "Json Error : {err}"),
         }
     }
 }
@@ -37,3 +47,14 @@ impl<M: UserSubscribeManage> From<reqwest::Error> for MobPushError<M> {
     }
 }
 
+impl<M: UserSubscribeManage> From<(u16, String)> for MobPushError<M> {
+    fn from((state, msg): (u16, String)) -> Self {
+        Self::Mob { state, msg }
+    }
+}
+
+impl<M: UserSubscribeManage> From<serde_json::Error> for MobPushError<M> {
+    fn from(err: serde_json::Error) -> Self {
+        Self::Json(err)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+mod config;
 mod error;
 mod pusher;
-mod config;
 
 mod pushing_data;
 mod user_subscribe;
 
 pub use pushing_data::PushEntity;
-pub use user_subscribe::{SubscribeFilter, UserSubscribeManage};
+pub use user_subscribe::{SubscribeFilter, UserMobId, UserSubscribeManage};
 
-pub use pusher::MobPusher;
 pub use config::set_config;
+pub use pusher::MobPusher;
