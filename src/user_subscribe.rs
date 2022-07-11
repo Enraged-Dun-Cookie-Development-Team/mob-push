5a0c63814d48d612be33e8e2f3f79833e2b835a7
Frozen String <964413011@qq.com>
2022-07-11 10:45:07 +0800

    ♻️ (subscribe) 调节接口参数名称

--- src/user_subscribe.rs
+++ src/user_subscribe.rs
@@ -4,8 +4,8 @@ use crate::PushEntity;
 
 type BoxResultFuture<T, E> = Pin<Box<dyn Future<Output = Result<T, E>> + Send + Sync>>;
 
-pub trait UserSubscribeManage:'static {
-    type UserIdentify: Sized + 'static + Send + Sync;
+pub trait UserSubscribeManage: 'static {
+    type UserIdentify: UserMobId;
     type PushData: PushEntity;
     type Filter: SubscribeFilter;
     type Err: From<<Self::Filter as SubscribeFilter>::Err>
@@ -22,12 +22,12 @@ pub trait UserSubscribeManage:'static {
     fn check_subscribed(
         &self,
         user_id: &Self::UserIdentify,
-        data_group: &<Self::PushData as PushEntity>::Resource,
+        data_resource: &<Self::PushData as PushEntity>::Resource,
     ) -> BoxResultFuture<bool, Self::Err>;
 
     fn fetch_all_subscriber(
         &self,
-        data_group: &<Self::PushData as PushEntity>::Resource,
+        data_resource: &<Self::PushData as PushEntity>::Resource,
     ) -> BoxResultFuture<Vec<Self::UserIdentify>, Self::Err>;
 }
 
@@ -38,4 +38,10 @@ pub trait SubscribeFilter: 'static + Send + Sync {
     fn filter(input: impl Iterator<Item = Self::Data>) -> Result<Vec<Self::Data>, Self::Err>;
 
     fn contains(target: &<Self::Data as PushEntity>::Resource) -> Result<bool, Self::Err>;
+}
+
+pub trait UserMobId: Sized + Send + Sync + 'static {
+    type MobId: ToString + 'static + Send + Sync + Sized;
+
+    fn get_mob_id(&self) -> Self::MobId;
 }
